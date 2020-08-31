367f323e09c94df4654148766904b2cf770c191f
KyWilkinson <wilki018@hotmail.com>
2020-08-31 13:53:46 -0500

    Created sweepstakes class

---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        private Dictionary<int, Contestant> contestants;
+        private string name;
+        public string Name;
+    }
+}
