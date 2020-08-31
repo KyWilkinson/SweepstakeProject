8fb77ad8cfccfcd2e652b13a0c19d007b9bd684e
KyWilkinson <wilki018@hotmail.com>
2020-08-31 14:25:04 -0500

    Created userinterface class

---
+++ Sweepstakes/SweepstakeQueueManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class SweepstakeQueueManager
+    {
+        private Queue<Sweepstakes> queue;
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+
+        }
+    }
+}
